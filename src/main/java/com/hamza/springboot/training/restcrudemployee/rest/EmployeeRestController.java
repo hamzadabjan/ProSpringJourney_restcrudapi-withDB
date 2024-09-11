319b566dbfd9f7933d0294ff4358621a322401ea
Hamza <hamza.dabjan@gmail.com>
2024-09-11 03:16:14 +0300

    full CRUD using spring DATA REST

--- src/main/java/com/hamza/springboot/training/restcrudemployee/rest/EmployeeRestController.java
+++ src/main/java/com/hamza/springboot/training/restcrudemployee/rest/EmployeeRestController.java
@@ -1,70 +1,70 @@
-package com.hamza.springboot.training.restcrudemployee.rest;
-
-
-import com.hamza.springboot.training.restcrudemployee.dao.EmployeeDAO;
-import com.hamza.springboot.training.restcrudemployee.entity.Employee;
-import com.hamza.springboot.training.restcrudemployee.service.EmployeeService;
-import netscape.javascript.JSObject;
-import org.apache.tomcat.util.json.JSONParser;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping ("/api")
-public class EmployeeRestController {
-
-    //private EmployeeDAO employeeDAO;
-
-    private EmployeeService employeeService;
-    @Autowired
-    public EmployeeRestController(EmployeeService theEmployeeService) {
-        employeeService = theEmployeeService;
-    }
-
-    @GetMapping ("/employees")
-    public List<Employee> findAll(){
-        return employeeService.findAll();
-    }
-
-    // add mapping for GET /employees/{employeeId}
-    @GetMapping ("/employees/{employeeId}")
-    public Employee getEmployee(@PathVariable int employeeId){
-        Employee theEmployee = employeeService.findById(employeeId);
-        if (theEmployee==null){
-            throw new RuntimeException("Employee id not found - "+employeeId);
-        }
-        return theEmployee;
-    }
-
-    // add mapping for POST /employees - add new employee
-    @PostMapping("/employees")
-    public Employee addEmployee(@RequestBody Employee theEmployee){
-        theEmployee.setId(0);
-        Employee dbEmployee = employeeService.save(theEmployee);
-        return dbEmployee;
-    }
-
-    // add mapping for PUT /employees - update existing employee
-    @PutMapping("/employees")
-    public Employee updateEmployee(@RequestBody Employee theEmployee){
-
-        Employee dbEmployee = employeeService.save(theEmployee);
-
-        return dbEmployee;
-    }
-
-    // add mapping for DELETE /employees/{employeeId} - delete existing employee
-    @DeleteMapping("/employees/{employeeId}")
-    public String deleteEmployee(@PathVariable int employeeId){
-        Employee tempEmployee = employeeService.findById(employeeId);
-        if (tempEmployee ==null){
-            throw new RuntimeException("the employee id not found -"+employeeId);
-        }
-        employeeService.deleteById(employeeId);
-
-        return "Deleted employee id - " + employeeId;
-    }
-
-}
+//package com.hamza.springboot.training.restcrudemployee.rest;
+//
+//
+//import com.hamza.springboot.training.restcrudemployee.dao.EmployeeDAO;
+//import com.hamza.springboot.training.restcrudemployee.entity.Employee;
+//import com.hamza.springboot.training.restcrudemployee.service.EmployeeService;
+//import netscape.javascript.JSObject;
+//import org.apache.tomcat.util.json.JSONParser;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping ("/api")
+//public class EmployeeRestController {
+//
+//    //private EmployeeDAO employeeDAO;
+//
+//    private EmployeeService employeeService;
+//    @Autowired
+//    public EmployeeRestController(EmployeeService theEmployeeService) {
+//        employeeService = theEmployeeService;
+//    }
+//
+//    @GetMapping ("/employees")
+//    public List<Employee> findAll(){
+//        return employeeService.findAll();
+//    }
+//
+//    // add mapping for GET /employees/{employeeId}
+//    @GetMapping ("/employees/{employeeId}")
+//    public Employee getEmployee(@PathVariable int employeeId){
+//        Employee theEmployee = employeeService.findById(employeeId);
+//        if (theEmployee==null){
+//            throw new RuntimeException("Employee id not found - "+employeeId);
+//        }
+//        return theEmployee;
+//    }
+//
+//    // add mapping for POST /employees - add new employee
+//    @PostMapping("/employees")
+//    public Employee addEmployee(@RequestBody Employee theEmployee){
+//        theEmployee.setId(0);
+//        Employee dbEmployee = employeeService.save(theEmployee);
+//        return dbEmployee;
+//    }
+//
+//    // add mapping for PUT /employees - update existing employee
+//    @PutMapping("/employees")
+//    public Employee updateEmployee(@RequestBody Employee theEmployee){
+//
+//        Employee dbEmployee = employeeService.save(theEmployee);
+//
+//        return dbEmployee;
+//    }
+//
+//    // add mapping for DELETE /employees/{employeeId} - delete existing employee
+//    @DeleteMapping("/employees/{employeeId}")
+//    public String deleteEmployee(@PathVariable int employeeId){
+//        Employee tempEmployee = employeeService.findById(employeeId);
+//        if (tempEmployee ==null){
+//            throw new RuntimeException("the employee id not found -"+employeeId);
+//        }
+//        employeeService.deleteById(employeeId);
+//
+//        return "Deleted employee id - " + employeeId;
+//    }
+//
+//}
--- src/main/java/com/hamza/springboot/training/restcrudemployee/service/EmployeeService.java
+++ src/main/java/com/hamza/springboot/training/restcrudemployee/service/EmployeeService.java
@@ -1,15 +1,15 @@
-package com.hamza.springboot.training.restcrudemployee.service;
-
-import com.hamza.springboot.training.restcrudemployee.entity.Employee;
-
-import java.util.List;
-
-public interface EmployeeService {
-    List<Employee> findAll();
-
-    Employee findById(int theId);
-
-    Employee save(Employee theEmployee);
-
-    void deleteById (int theId);
-}
+//package com.hamza.springboot.training.restcrudemployee.service;
+//
+//import com.hamza.springboot.training.restcrudemployee.entity.Employee;
+//
+//import java.util.List;
+//
+//public interface EmployeeService {
+//    List<Employee> findAll();
+//
+//    Employee findById(int theId);
+//
+//    Employee save(Employee theEmployee);
+//
+//    void deleteById (int theId);
+//}
