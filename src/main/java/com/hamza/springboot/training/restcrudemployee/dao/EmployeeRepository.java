87b3eee550154d8b26d3a6633649e8b5cdae7e3c
Hamza <hamza.dabjan@gmail.com>
2024-09-11 03:38:30 +0300

    full CRUD using spring DATA REST - configuration and pagination

--- src/main/java/com/hamza/springboot/training/restcrudemployee/dao/EmployeeRepository.java
+++ src/main/java/com/hamza/springboot/training/restcrudemployee/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.hamza.springboot.training.restcrudemployee.dao;
 
 import com.hamza.springboot.training.restcrudemployee.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource (path="members")
 public interface EmployeeRepository extends JpaRepository <Employee , Integer> {
 
 }
